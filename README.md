b3b127cedbca0dc032757fba959834b5ec5dd2c1
Aarush544 <singhaarush620@gmail.com>
2025-11-02 23:43:12 +0000

    Changed the readme and polished code

--- README.md
+++ README.md
@@ -5,42 +5,17 @@
 - NPC generation made easy
 - Simple and efficient
 - Satisfying animations
+- Can make as much NPCs as you want
+- Have control over name and gender
+- Problem of having to make a large amount of diverse NPCs handled
+- 6 different attributes
+- Cool printing animation that adds flair
+- Easily expandable to add more attributes.
 
 ## ℹ️ Overview
 
-This is my NPC generator that I made it as a faster and more efficient solution to this problem. Also I 
+This is my NPC generator that I made as a faster and more efficient solution to this problem. It functions by first inputting the amount of NPCs that you want to generate. Then you input the name and gender of the NPC and get the rest of the attributes given to you. It will output the height, hostility, name, gender, profession, and personality of the NPC. While doing this, it will play a scrambling animation that cycles through the random possibilities before giving you the attributes. This creates a unique generation experience. I am Aarush Singh and I made it so that people could quickly generate NPCs that are diverse and unique. Having to make lots of NPCs by hand and have them all be unique can be difficult for game development and this provides an effective solution.
+ 
+### ✍️ Author
 
-### ✍️ Authors
-
-I am Aarush Singh 
-
-
-## 🚀 Usage
-
-*Show off what your software looks like in action! Try to limit it to one-liners if possible and don't delve into API specifics.*
-
-```py
->>> import mypackage
->>> mypackage.do_stuff()
-'Oh yeah!'
-```
-
-
-## ⬇️ Installation
-
-Simple, understandable installation instructions!
-
-```bash
-pip install my-package
-```
-
-And be sure to specify any other minimum requirements like Python versions or operating systems.
-
-*You may be inclined to add development instructions here, don't.*
-
-
-## 💭 Feedback and Contributing
-
-Add a link to the Discussions tab in your repo and invite users to open issues for bugs/feature requests.
-
-This is also a great place to invite others to contribute in any ways that make sense for your project. Point people to your DEVELOPMENT and/or CONTRIBUTING guides if you have them.
+I am Aarush Singh, a student of the CSAEA. I made this program to solve the problem of having to tediously hand craft each NPC for video game development. This program brings automation to NPC generation, giving people the opportunity to easily generate interesting NPCs with the click of a button. This project was also a way for me to practice basic python skills like loops, conditionals, and variables.
--- npc_generator.py
+++ npc_generator.py
@@ -4,9 +4,8 @@ import random
 #Lists used for attributes and animation
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-personality_options = ["kind", "intellegent", "courageous", "skilled", "loyal", "creative", "calm"]        
+personality_options = ["kind", "intellegent", "courageous", "skilled", "crafty", "creative", "calm"]        
 professions = ["blacksmith", "farmer", "mason", "butcher", "fletcher"]
-genders = ["male", "female", "transgender"]
 
 
 # Prints slowly so it looks cool
@@ -17,6 +16,7 @@ def cool_print(word_to_print):
         time.sleep(0.1)
 
 class NPC:
+    # initializes class
     def __init__(self, name, gender, height, personality, profession, hostility):
         self.name = name
         self.gender = gender
@@ -25,35 +25,36 @@ class NPC:
         self.profession = profession
         self.hostility = hostility
     
-    # Makes cool animation and outputs result
+    # Makes cool scrambling animation  
 
-    def random_animation(self):
+    def random_animation(self, name, gender, height, personality, profession, hostility ):
         global personality_options
         global professions
-        global i
         begin_time = time.time()
         duration = 1.2
         while True:
-            rand_name = f"{random.choice(alphabet)}{random.choice(alphabet)}{random.choice(alphabet)}{random.choice(alphabet)}{random.choice(alphabet)}{random.choice(alphabet)}"
-            rand_gender = f"{random.choice(genders)}"
-            rand_height = f"{random.randint(4, 7)}\"{random.randint(1, 12)}"
-            rand_personality = f"{random.choice(personality_options)}"
-            rand_profession = f"{random.choice(professions)}"
-            print(f"\r{rand_name} is a {rand_gender} person who is {rand_height}. {rand_name} is a {rand_personality} {rand_profession}.", end = "", flush=True)
+            #changes the variable to give scrambling animation
+            self.name = f"{random.choice(alphabet)}{random.choice(alphabet)}{random.choice(alphabet)}{random.choice(alphabet)}{random.choice(alphabet)}{random.choice(alphabet)}"
+            self.gender = f"{random.choice(["male", "female"])}"
+            self.height = f"{random.randint(4, 7)}\"{random.randint(1, 12)}"
+            self.personality = f"{random.choice(personality_options)}"
+            self.profession = f"{random.choice(professions)}"
+            self.hostility = f"{random.choice(["hostile", "friendly"])}"
+            print(f"\r{self.name} is a {self.gender} person who is {self.height}. They are a {self.personality} {self.profession} who is {self.hostility} to you.", end = "", flush=True)
             
+            #exits after certain amount of time
+
             if (time.time() - begin_time) > duration:
                 break
             time.sleep(0.01)
-        print("\r                                                                                              ")
-        print(f"{self.name} is a {self.gender} person who is {self.height}. {self.name} is a {self.personality} {self.profession}.", flush=True)
-
-
-
+        print("\r                                                                                                         ")
+        
+        
 # Asks use for gender of NPC with error handling. Loops until valid input
 
 def gender_check():
     cool_print("Enter the corresponding number for the gender:\n1. Male\n2. Female\nAnswer: ")
-    gender_input = (input()).strip()
+    gender_input = (input())
     while gender_input != '1' and gender_input != '2':   
         cool_print("Invalid")
         cool_print("Enter the corresponding number for the gender of the NPC:\n1. Male\n2. Female\nAnswer: ")
@@ -61,24 +62,34 @@ def gender_check():
     output = "male" if gender_input == "1" else "female"  
     return output
 
+
 # Asks for number of NPCs
 
 cool_print("Enter the amount of NPCs you want to generate: ")
 count = int(input(""))
-
+   
 # main part that loops for each NPC 
 
 for i in range(1, count + 1):
-    character = NPC(0,0,0,0,0)
+    character = NPC(0,0,0,0,0,0)
     
-    #Gets all of the attributes
-    cool_print("Enter NPC name: ")
+    # Gets all of the attributes
+    print("___________________________________________________________")
+    cool_print("\nEnter NPC name: ")
     character.name = input("")
     character.gender = gender_check()
-    character.height = f"{random.randint(5, 6)}\" {random.randint(1, 11)}"
+    character.height = f"{random.randint(50, 80)} inches"
     character.personality = random.choice(personality_options)
     character.profession = random.choice(professions)
-    character.random_animation()
-  
-   
+    character.hostility = random.choice(["hostile", "friendly"]) 
     
+    pronoun = "He" if character.gender == "male" else "She"
+
+    answer = f"{character.name} is a {character.gender} person who is {character.height} tall. {pronoun} is a {character.personality} {character.profession} who is {character.hostility} to you."
+    character.random_animation(0, 0, 0, 0, 0, 0)
+    
+    #Outputs the result
+
+    print(f"NPC #{i}")
+    print("\r")
+    print(answer, flush = True)
